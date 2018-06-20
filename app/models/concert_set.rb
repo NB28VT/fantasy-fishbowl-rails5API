b0e2ad155dca7525e25fe08f235f8b3d3ee35860
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro.local>
2018-06-20 19:44:18 -0400

    Concert Set basically functionality

--- app/models/concert_set.rb
+++ app/models/concert_set.rb
@@ -1,2 +1,6 @@
 class ConcertSet < ApplicationRecord
+  belongs_to :concert
+
+  validates_presence_of :set_number
+  validates_presence_of :concert_id
 end
--- spec/models/concert_set_spec.rb
+++ spec/models/concert_set_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ConcertSet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to(:concert)}
+
+  it {should validate_presence_of(:set_number)}
+  it {should validate_presence_of(:concert_id)}
 end
