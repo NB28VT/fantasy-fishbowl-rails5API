bd913b54a9c424e07d11be450cd0cfe82cbf5461
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro.local>
2018-06-21 08:24:51 -0400

    Passing concert index tests

--- spec/requests/concerts_spec.rb
+++ spec/requests/concerts_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "Concerts", type: :request do
-  describe "GET /upcoming_concerts" do
+  describe "GET /concerts" do
     it "raises an error if a user is not authenticated" do
-      get "/upcoming_concerts"
+      get "/concerts"
       expect(response.response_code).to eq(401)
     end
 
@@ -13,35 +13,46 @@ RSpec.describe "Concerts", type: :request do
 
       token = sign_in_user(user)
 
-      get "/upcoming_concerts", {}, {Authorization: token}
+      get "/concerts", params: {}, headers: {Authorization: token}
 
-      expect(json.count).to eq(2)
-      expect(json.first["id"]).to eq(concerts.first.id)
-      expect(json.first["show_date"]).to eq(concerts.first.show_date)
-      expect(json.first["venue_name"]).to eq(concerts.first.venue_name)
+      expect(json["concerts"].count).to eq(2)
+      expect(json["concerts"].first).to eq(
+        {
+          "id" => concerts.first.id,
+          "show_date" => concerts.first.show_date.strftime("%m/%d/%Y"),
+          "venue_name" => concerts.first.venue_name
+        }
+      )
     end
 
-    it "returns concerts in chronological order" do
+    it "returns concerts in chronological order by show date" do
       user = create(:user)
       create(:concert, show_date: 2.days.from_now)
       create(:concert, show_date: 1.days.from_now)
       token = sign_in_user(user)
 
-      get "/upcoming_concerts", params: {authentication_token: token}
+      get "/concerts", params: {authentication_token: token}
 
       expect(json.first.show_date).to be < json.second.show_date
     end
+
+    context "when only upcoming concerts are requested" do
+      it "returns a list of upcoming concerts"
+      it "does not return any concerts that have gone by"
+    end
+
   end
 
   describe "GET /concerts/:id" do
-    it "returns the concerts venue data"
-    it "returns the concert's sets ordered by setlist position"
-    it "returns the concerts song performances ordered by setlist position"
-  end
+    context "when a show has already occured" do
+      it "returns the concerts venue data"
+      it "returns the concert's sets ordered by setlist position"
+      it "returns the concerts song performances ordered by setlist position"
+    end
 
-  describe "GET /concerts" do
-    it "raises an error if a user is not authenticated"
-    it "returns a list of all concerts"
+    context "when a show is yet to occur" do
+      it "returns blank data for concert sets and song performances"
+    end
   end
 
   describe "POST /concerts" do
