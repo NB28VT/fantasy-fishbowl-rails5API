70fae1dde767a8886d5092cd60b8a10d1e976566
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro.local>
2018-06-24 22:14:26 -0400

    String up basic React Router

---
+++ client/src/Login.js
@@ -0,0 +1,12 @@
+import{ Component } from 'react';
+
+class Login extends Component {
+    constructor(props) {
+      super();
+    }
+
+
+
+}
+
+export default Login;
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './Login';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={App} />
+      <Route path="/login" component={Login}/>
+    </div> 
+  </BrowserRouter>,
+
+document.getElementById('root'))
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
