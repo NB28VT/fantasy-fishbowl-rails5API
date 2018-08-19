9b39c784c7bcd0b83b181d4e53a3cef255e388eb
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2018-08-19 12:18:08 -0400

    Placeholder sign in link on landing page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
 import WelcomeLogo from "./WelcomeLogo";
 import '../stylesheets/landing-page.css';
 
@@ -10,6 +11,7 @@ class LandingPage extends Component {
         <WelcomeLogo />
         <p>Fantasy Fishbowl is currently under construction.</p>
         <p>Please contact burgess.nathan28@gmail.com for inquiries about obtaining a login.</p>
+        <p>Got a login? Sign in</p><Link to="/login">Here</Link>
       </div>
     )
   }
