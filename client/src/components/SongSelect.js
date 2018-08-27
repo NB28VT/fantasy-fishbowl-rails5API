10f3d31a92a6b37acef7b4d56cdceeecc12b8556
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2018-08-27 08:34:24 -0400

    Try select pacakge

--- client/src/components/SongSelect.js
+++ client/src/components/SongSelect.js
@@ -1,74 +1,114 @@
 import React, { Component } from "react";
-import Downshift from "downshift";
-
+import Select from "react-select";
 class SongSelect extends Component {
-  component(props) {
+  constructor(props) {
+    super(props)
     this.state = {
-      songID: null,
-      songName: ""
+      selectedOption: null,
+      songOptions: []
+    }
+  }
+
+  componentDidMount() {
+    console.log("did mount")
+    if (this.props.songs.length > 0) {
+      const options = this.props.songs.map((song) =>
+        ({value: song.id, label: song.name})
+      )
+
+      this.setState({songOptions: options})
     }
 
-    this.updateSelection = this.updateSelection.bind(this)
   }
 
-  updateSelection(selection) {
-    console.log("Updating shit")
-    this.setState({
-      songID: selection.id,
-      songName: selection.name
-    })
-  }
+  handleChange = (selectedOption) => {
+     this.setState({ selectedOption });
+     console.log(`Option selected:`, selectedOption);
+   }
 
+   render() {
+     const { selectedOption, songOptions } = this.state;
 
-
-  render() {
-    return(
-      <Downshift
-        onChange={selection => this.updateSelection(selection)}
-        itemToString={item => (item ? item.value : '')}
-      >
-        {({
-          getInputProps,
-          getItemProps,
-          getLabelProps,
-          isOpen,
-          inputValue,
-          highlightedIndex,
-          selectedItem,
-        }) => (
-          <div>
-            <label {...getLabelProps()}>{this.props.category}</label>
-            <input {...getInputProps()} />
-            {isOpen ? (
-              <div>
-                {this.props.songs
-                  .filter(item => !inputValue || item.name.toLowerCase().includes(inputValue.toLowerCase()))
-                  .map((item, index) => (
-                    <div
-                      {...getItemProps({
-                        key: item.name,
-                        id: item.id,
-                        index,
-                        item,
-                        style: {
-                          backgroundColor:
-                            highlightedIndex === index ? 'lightgray' : 'white',
-                          fontWeight: selectedItem === item ? 'bold' : 'normal',
-                          color: "black"
-                        },
-                      })}
-                    >
-                      {item.name}
-                    </div>
-                  ))}
-              </div>
-            ) : null}
-          </div>
-        )}
-      </Downshift>
-    )
-  }
+     return (
+       <Select
+         value={selectedOption}
+         onChange={this.handleChange}
+         options={songOptions}
+       />
+     );
+   }
 }
+
+// import Downshift from "downshift";
+//
+// class SongSelect extends Component {
+//   component(props) {
+//     this.state = {
+//       songID: null,
+//       songName: ""
+//     }
+//
+//     this.updateSelection = this.updateSelection.bind(this)
+//   }
+//
+//   updateSelection(selection) {
+//     console.log("Updating shit")
+//     this.setState({
+//       songID: selection.id,
+//       songName: selection.name
+//     })
+//   }
+//
+//
+//
+//   render() {
+//     return(
+//       <Downshift
+//         onChange={selection => this.updateSelection(selection)}
+//         itemToString={item => (item ? item.value : '')}
+//       >
+//         {({
+//           getInputProps,
+//           getItemProps,
+//           getLabelProps,
+//           isOpen,
+//           inputValue,
+//           highlightedIndex,
+//           selectedItem,
+//         }) => (
+//           <div>
+//             <label {...getLabelProps()}>{this.props.category}</label>
+//             <input {...getInputProps()} />
+//             {isOpen ? (
+//               <div>
+//                 {this.props.songs
+//                   .filter(item => !inputValue || item.name.toLowerCase().includes(inputValue.toLowerCase()))
+//                   .map((item, index) => (
+//                     <div
+//                       {...getItemProps({
+//                         key: item.name,
+//                         id: item.id,
+//                         index,
+//                         item,
+//                         style: {
+//                           backgroundColor:
+//                             highlightedIndex === index ? 'lightgray' : 'white',
+//                           fontWeight: selectedItem === item ? 'bold' : 'normal',
+//                           color: "black"
+//                         },
+//                       })}
+//                     >
+//                       {item.name}
+//                     </div>
+//                   ))}
+//               </div>
+//             ) : null}
+//           </div>
+//         )}
+//       </Downshift>
+//     )
+//   }
+// }
 
 
 
