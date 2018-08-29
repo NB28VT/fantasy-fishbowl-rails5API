285a1efff31402717371ec91d34600e081422a6a
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2018-08-29 09:38:17 -0400

    Hoisted song selections up to PredictionForm component

--- client/src/components/PredictionForm.js
+++ client/src/components/PredictionForm.js
@@ -14,10 +14,13 @@ class PredictionForm extends Component {
     super(props)
     this.state = {
       predictionCategories: [],
-      songs: []
+      songs: [],
+      categorySelections: []
     }
     this.fetchPredictionCategories = this.fetchPredictionCategories.bind(this)
     this.fetchSongList = this.fetchSongList.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.updateSelection = this.updateSelection.bind(this)
   }
 
   componentDidMount() {
@@ -32,8 +35,14 @@ class PredictionForm extends Component {
     .then((res) => res.json())
     .then((responseData) => {
       // TODO: error handling
+      const categorySelections = {}
+      responseData["prediction_categories"].map((category) =>
+        categorySelections[category.id] = {song_id: 1}
+      )
+
       this.setState({
-        predictionCategories: responseData["prediction_categories"]
+        predictionCategories: responseData["prediction_categories"],
+        categorySelections: categorySelections
       })
     })
   }
@@ -51,15 +60,50 @@ class PredictionForm extends Component {
     })
   }
 
+  buildPredictionSubmission() {
+    debugger;
+  }
+
+  updateSelection(songID, categoryID) {
+    // Possibly tie to state in selection for each. But does React Bootstrap take care of that for us?
+
+    const categorySelections = {...this.state.categorySelections}
+    categorySelections[categoryID]["song_id"] = Number(songID)
+    this.setState({
+      categorySelections: categorySelections
+    })
+  }
+
+  handleSubmit() {
+    const endpoint = `concerts/${this.props.concertID}/predictions`;
+    // Need auth header for token (backend assigns token ID)
+    
+
+
+
+
+    debugger;
+    // post "/concerts/#{concert.id}/predictions", params: {
+    //   concert_prediction: {
+    //     song_predictions_attributes: [
+    //       {
+    //         song_id: songs.first.id,
+    //         prediction_category_id: prediction_category.id
+    //       }
+    //     ]
+    //   }
+    // debugger;
+  }
+
   render() {
     const categories = this.state.predictionCategories.map((category) =>
       <Col className="prediction-category" key={category.id} sm={10} >
-        <SongSelect categoryName={category.name} songs={this.state.songs}/>
+        <SongSelect categoryName={category.name} categoryID={category.id} updateSelectionForm={this.updateSelection} songs={this.state.songs}/>
       </Col>
     )
 
     return(
-      <Form horizontal className="prediction-form">
+      <Form horizontal className="prediction-form" onSubmit={this.handleSubmit}>
         <FormGroup>
           {categories}
         </FormGroup>
--- client/src/components/SongSelect.js
+++ client/src/components/SongSelect.js
@@ -6,6 +6,17 @@ import {
 } from 'react-bootstrap';
 
 class SongSelect extends Component {
+  constructor(props) {
+    super(props)
+    this.updateSelection = this.updateSelection.bind(this)
+  }
+
+  updateSelection(e) {
+    const songID = e.target.value;
+    this.props.updateSelectionForm(songID, this.props.categoryID);
+  }
+
+  // TODO: keep track of selected here, keep state in prediction form component
   render() {
     const songOptions = this.props.songs.map((song) => {
       return(<option key={song.id} value={song.id}>{song.name}</option>)
@@ -14,7 +25,7 @@ class SongSelect extends Component {
     return(
       <FormGroup>
         <ControlLabel>{this.props.categoryName}</ControlLabel>
-        <FormControl componentClass="select" placeholder="Select">
+        <FormControl componentClass="select" placeholder="Select" onChange={this.updateSelection}>
           {songOptions}
         </FormControl>
       </FormGroup>
