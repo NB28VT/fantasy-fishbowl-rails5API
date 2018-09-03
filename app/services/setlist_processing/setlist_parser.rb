3e110aa0ce4ac069612577c48793bc30b85ec4c3
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2018-09-03 13:59:48 -0400

    Refactor setlist parser

--- app/services/setlist_processing/setlist_parser.rb
+++ app/services/setlist_processing/setlist_parser.rb
@@ -8,39 +8,43 @@ module SetlistProcessing
 
     def parse
       validate_setlist_data
-      setlist = find_setlist
-      document = Nokogiri::HTML(setlist)
-      sets = document.search("p")
+      parsed_html = Nokogiri::HTML(setlist_data)
+      sets = parsed_html.search("p")
       raise ApiParseError, "No set paragraphs found" if sets.blank?
 
-      concert_sets = sets.map.with_index{|set_data, set_index| build_concert_set(set_data, set_index)}
+      concert_sets = sets.map.with_index do |set_data, set_number|
+        {
+          set_number: set_number,
+          song_performances: build_song_performances(set_data)
+        }
+      end
+
       return {concert_sets: concert_sets}
     end
 
     private
 
     def validate_setlist_data
-      raise ApiParseError, "Error Pulling Setlist: #{@concert_json["error_message"]}" if @concert_json["error_message"].present?
-      raise ApiParseError, "Setlist Not Found" if @concert_json["response"]["count"] == 0
+      raise ApiParseError, "Phish.net Error Message: #{@concert_json["error_message"]}" if @concert_json["error_message"].present?
+      raise ApiParseError, "Empty Search Results" if @concert_json["response"]["count"] == 0
+      raise ApiParseError, "Empty Response Data" if @concert_json.dig("response", "data").blank?
+      raise ApiParseError, "Missing Setlist Data" if @concert_json.dig("response", "data").find{|data| data.has_key?("setlistdata")}.nil?
+    end
+
+    def setlist_data
+      concert_result = @concert_json["response"]["data"].first
+      return concert_result["setlistdata"]
     end
 
     def build_concert_set(set_data, set_index)
-      song_performances = build_song_performances(set_data)
-      return {set_number: set_index, song_performances: song_performances}
+      return {
+        set_number: set_index,
+        song_performances: build_song_performances(set_data)
+      }
     end
 
     def build_song_performances(set_data)
-      return set_data.search("a").map.with_index{|song, index|  {song_name: song.text, setlist_position: index}}
-    end
-
-    def find_setlist
-      response_data = @concert_json.dig("response", "data")
-      raise ApiParseError, "Empty response data" if response_data.blank?
-
-      concert_data = response_data.find{|data| data.has_key?("setlistdata")}
-      raise ApiParseError, "Missing setlist data" if concert_data.blank?
-
-      return concert_data["setlistdata"]
+      return set_data.search("a").map.with_index{|song, setlist_position|  {song_name: song.text, setlist_position: setlist_position}}
     end
   end
 end
