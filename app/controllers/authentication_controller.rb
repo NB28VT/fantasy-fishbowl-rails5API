e3797192a1ebd1f5bfda3d995482a1dedb5f9c7b
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2019-07-01 08:55:48 -0400

    Revert to rescuing from standard error on failed login

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,10 +3,9 @@ class AuthenticationController < ApplicationController
   skip_before_action :authenticate_request
 
   def authenticate
-    # raise AuthenticationError, "Shit borked here"
     token = UserAuthenticator.new(params[:email], params[:password]).generate_auth_token
     render json: {token: token}, status: 200
-  rescue AuthenticationError => e
+  rescue StandardError => e
     render json: {errors: e.message}, status: 401
   end
 
