08b9669d9923824b8028405f5f14fe720869f60b
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2019-12-25 11:43:20 -0500

    Build tests for TDDing rankings calculator

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
     predictions = ConcertPrediction.where(user_id: params[:id])
     render json: predictions, root: "concert_predictions"
   end
+
+  def leaderboard
+    # init leaderboard calculation service
+    # pass in all users
+    # serialize in a list ordered by the rankings top to bottom
+  end
 end
---
+++ spec/services/rankings_calculator.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "RankingsCalculator" do
+    describe "#get_rankings" do
+        it "returns a list of rankings"
+        it "returns an empty array if no users are present"
+        it "returns all zero scores if no predictions have been scored"
+    end
+
+end
+
+
+
