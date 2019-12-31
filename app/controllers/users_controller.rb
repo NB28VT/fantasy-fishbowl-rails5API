9ac1742efec437ef9d0cd13b084654fe515c2f3e
Nathan Burgess <nathan.burgess28@hotmail.com>
2019-12-31 08:18:00 -0500

    Skip authentication for leaderboard endpoint for now (#30)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  # TODO: Implement client-side authentication and fix this
+  skip_before_action :authenticate_request
+
   def predictions
     predictions = ConcertPrediction.where(user_id: params[:id])
     render json: predictions, root: "concert_predictions"
