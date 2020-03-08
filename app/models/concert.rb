b90d1e96ea37b002d2200bc72d35e306e3931ae4
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2020-03-08 10:29:03 -0400

    [Concerts] Add Concert Tour model, Concerts association

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -3,6 +3,8 @@ class Concert < ApplicationRecord
   has_many :concert_predictions, dependent: :destroy
   has_many :song_performances, through: :concert_sets, dependent: :destroy
 
+  belongs_to :concert_tour
+
   validates_presence_of :venue_name
   validates_presence_of :show_time
 
