c0d401afea8f54c29093c788ea5d8973980ae6a3
Nathan Burgess <nathan.burgess28@hotmail.com>
2020-04-27 09:48:49 -0400

    Upgrade Rails and store venue images with Active Storage

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -3,6 +3,8 @@ class Concert < ApplicationRecord
   has_many :concert_predictions, dependent: :destroy
   has_many :song_performances, through: :concert_sets, dependent: :destroy
 
+  has_one_attached :venue_image
+
   belongs_to :concert_tour
 
   validates_presence_of :venue_name
--- app/serializers/concert_serializer.rb
+++ app/serializers/concert_serializer.rb
@@ -1,8 +1,13 @@
 class ConcertSerializer < ActiveModel::Serializer
-  attributes :id, :show_time, :venue_name
+
+  attributes :id, :show_time, :venue_name, :venue_image_src
 
   def show_time
     object.show_time.strftime("%m/%d/%Y")
+  end
+
+  def venue_image_src
+    rails_blob_path(object.venue_image, disposition: "attachment") if object.venue_image.attachment
   end
 
   has_many :concert_sets
